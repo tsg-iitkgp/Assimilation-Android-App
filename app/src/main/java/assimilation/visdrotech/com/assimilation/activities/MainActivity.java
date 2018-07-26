2460c707ccd2fb75d7377063ea77a36054c444b9
Ayush Goyal <ayushgoyal.iitkgp@gmail.com>
2018-07-27 03:42:07 +0530

    Change login page ui

--- app/src/main/java/assimilation/visdrotech/com/assimilation/activities/MainActivity.java
+++ app/src/main/java/assimilation/visdrotech/com/assimilation/activities/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -39,7 +40,7 @@ import retrofit2.Response;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private String prefName =  ((baseApplicationClass) this.getApplication()).PREF_NAME ;
 
-    private EditText username,password;
+    private TextInputEditText username, password;
     private Button signin;
     private ProgressBar loginProgress;
     private final static String TAG = "mainActivity" ;
@@ -207,8 +208,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
     private void initialiseVariables() {
-        username = (EditText) findViewById(R.id.username);
-        password = (EditText) findViewById(R.id.password);
+        username = (TextInputEditText) findViewById(R.id.username);
+        password = (TextInputEditText) findViewById(R.id.password);
         signin = (Button) findViewById(R.id.signin);
         loginProgress = (ProgressBar) findViewById(R.id.login_progress);
         signin.setOnClickListener(this);
