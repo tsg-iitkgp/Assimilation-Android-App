dfbbf04a2b3f9736cefda34b368e0ebf0b5e15b3
Ayush Goyal <ayushgoyal.iitkgp@gmail.com>
2018-07-28 03:00:34 +0530

    Adds permission check

--- app/src/main/java/assimilation/visdrotech/com/assimilation/activities/homepage.java
+++ app/src/main/java/assimilation/visdrotech/com/assimilation/activities/homepage.java
@@ -1,13 +1,19 @@
 package assimilation.visdrotech.com.assimilation.activities;
 
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -21,6 +27,9 @@ import android.widget.TextView;
 
 import assimilation.visdrotech.com.assimilation.R;
 import assimilation.visdrotech.com.assimilation.utils.baseApplicationClass;
+
+import static android.Manifest.permission.CAMERA;
+import static android.Manifest.permission.INTERNET;
 
 public class homepage extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -51,7 +60,58 @@ public class homepage extends AppCompatActivity
 
         }
         initialiseVariable();
+        if (checkPermission()) {
+
+        }else {
+            requestPermission();
+        }
+
         displayFragment(R.id.nav_upcomingevent);
+
+
+
+    }
+
+    private boolean checkPermission() {
+//        int result = ContextCompat.checkSelfPermission(getApplicationContext(), INTERNET);
+        int result1 = ContextCompat.checkSelfPermission(getApplicationContext(), CAMERA);
+
+        return result1 == PackageManager.PERMISSION_GRANTED;
+    }
+    private void requestPermission() {
+
+        ActivityCompat.requestPermissions(this, new String[]{ CAMERA}, 200);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 200:
+                if (grantResults.length > 0) {
+
+//                    boolean locationAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    boolean cameraAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+
+
+//                        Snackbar.make(view, "Permission Granted, Now you can access location data and camera.", Snackbar.LENGTH_LONG).show();
+                    if (!(cameraAccepted)) {
+
+//                        Snackbar.make(view, "Permission Denied, You cannot access location data and camera.", Snackbar.LENGTH_LONG).show();
+
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                            if (shouldShowRequestPermissionRationale(CAMERA)) {
+                                requestPermission();
+                                return;
+                            }
+                        }
+
+                    }
+                }
+
+
+                break;
+        }
     }
 
     private void initialiseVariable(){
